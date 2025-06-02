66f6c32c3003a3cc3878c95fe5474f9700acad70
Arindam01gi <1304arindam@gmail.com>
2025-06-02 23:20:25 +0530

    Add react-router-dom for routing; update App component to use Router and define routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./Home";
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div className='text-red-500'>Hello</div>
+      {/* <header className="bg-[var(--header-button-color)] text-[var(--text-color)] p-4">
+        <h1 className="text-2xl font-bold">Welcome to My Website</h1>
+        <nav className="mt-2">
+          <a href="#" className="text-[var(--link-highlight-color)] hover:underline">Home</a>
+          <a href="#" className="text-[var(--link-highlight-color)] hover:underline ml-4">About</a>
+          <a href="#" className="text-[var(--link-highlight-color)] hover:underline ml-4">Contact</a>
+        </nav>
+      </header>
+      <div className='text-red-500'>Hello</div> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
