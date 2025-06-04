f28e9d3db1b715a34776450ed0df93cc865f9a51
Arindam01gi <arindammaiti2018@gmail.com>
2025-06-04 23:07:23 +0530

    Update index.html to include Google Fonts; remove package-lock.json; update pnpm-lock.yaml with dependency version changes; modify index.css for new color variables and global font; enhance Navbar component layout and styling.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-     <div className='bg-[var(--primary-color)] text-[var(--background-color)]  h-10 w-full'>
-        Navbar
-     </div>
-  )
+   return (
+      <div className='bg-[var(--primary-color)] text-[var(--background-color)]  h-28 w-full flex items-center text-[22px]'>
+         <div className='flex justify-center items-center  gap-10 w-5/12 '>
+            <span>Apply</span>
+            <span>Visit</span>
+            <span>Contact</span>
+         </div>
+         <div className='w-1/6 flex justify-center items-center '>
+            <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/07/putney-high-mark-white.svg" alt="logo" className='w-16 h-16 hover:scale-110 transition-transform duration-300' />
+         </div>
+         <div className='w-1/3 flex justify-end  gap-10 '>
+            <span>Search</span>
+            <span>Menu</span>
+         </div>
+      </div>
+   )
 }
 
 export default Navbar
