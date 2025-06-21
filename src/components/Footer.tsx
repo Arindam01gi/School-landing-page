de42497de2b77344ae770a35b817ebfd47311a96
Arindam01gi <arindammaiti2018@gmail.com>
2025-06-21 22:32:49 +0530

    Enhance Footer component with improved layout and social media links; add Font Awesome for icon support and update styling for better responsiveness.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,53 +2,126 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='h-[42.5rem] bg-primary flex justify-center  items-center '>
+        <div className='px-8  space-y-12 md:space-y-24  w-full bg-primary  flex flex-col justify-center  items-center '>
 
-            <div className='w-[74%] h-full  flex flex-col'>
+            <div className=' h-full  flex flex-col items-center justify-center max-w-7xl '>
 
-                <div className=' w-full h-1/3 flex justify-center items-center'>
-                    <img
-                        src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/07/putney-high-logo-white.svg"
-                        alt="logo"
-                        className='w-auto h-36 transition-transform duration-300 hover:scale-110'
-                    />
+
+                <div className="flex justify-center my-16" data-aos="fade-in" data-aos-delay="300">
+                    <a href="https://www.putneyhigh.gdst.net" className="group">
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/07/putney-high-logo-white.svg" className="w-auto h-36 transform duration-300 ease-in-out group-hover:scale-105" alt="Homepage" />
+                    </a>
                 </div>
 
-                <div className=' w-full h-1/3 flex '>
+                <div className="flex flex-col md:grid md:grid-cols-12 gap-8 md:gap-16 text-center text-white aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                    <div className="space-y-4 prose col-span-full lg:col-start-4 lg:col-span-6">
+                        <h4><strong>Academic with a very modern edge</strong></h4>
+                        <p><span data-contrast="auto">Putney is a dynamic and down-to-earth school that nurtures bright and imaginative students.</span><span data-ccp-props="{&quot;201341983&quot;:0,&quot;335559739&quot;:160,&quot;335559740&quot;:259}">&nbsp;</span><span data-contrast="auto">Our unique brand of modern scholarship provides an outstanding education that is bold, broad and above all, relevant.</span></p>
 
-                    <div className=' w-1/3 h-full text-white flex flex-col justify-center items-center'>
-                        <p className='font-semibold'>Admissions</p>
-                        <div className='mt-6 flex flex-col justify-center items-center'>
-                            <p> Apply to Putney</p>
-                            <p>Open Days</p>
-                            <p>contact</p>
-                        </div>
                     </div>
-                    <div className=' w-1/3 h-full flex flex-col justify-center items-center text-white'>
-                        <div className='text-xl text-center font-semibold'>
-                            Academic with a very modern edge
-                        </div>
-                        <div className='text-center mt-6'>
-                            Putney is a dynamic and down-to-earth school that nurtures bright and imaginative students. Our unique brand of modern scholarship provides an outstanding education that is bold, broad and above all, relevant.
-                        </div>
+                    <div className="space-y-4 col-span-full sm:col-span-6 lg:col-start-1 lg:row-start-1 lg:col-span-3">
+                        <p className="font-bold">Admissions</p>
+                        <ul className="relative group text-pretty" data-menu="">
+                            <li className="py-1 md:py-0" data-menu-id="3170" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/admissions/joining-putney-high/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Apply to Putney
+                                </a>
+                            </li>
+                            <li className="py-1 md:py-0" data-menu-id="2587" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/admissions/open-days/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Open Days
+                                </a>
+                            </li>
+                            <li className="py-1 md:py-0" data-menu-id="2588" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/contact/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <div className=' w-1/3 h-full text-white flex flex-col justify-center items-center'>
-                        <p className='font-semibold'>Useful Information</p>
-                        <div className='mt-6 flex flex-col justify-center items-center'>
-                            <p> Term Dates</p>
-                            <p>Facilities Hire</p>
-                            <p>Current Vacancies</p>
-                        </div>
+                    <div className="space-y-4 col-span-full sm:col-span-6 lg:col-span-3 ">
+                        <p className="font-bold">Useful Information</p>
+                        <ul className="relative group text-pretty" data-menu="">
+                            <li className="py-1 md:py-0" data-menu-id="2589" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/school-life/term-dates/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Term Dates
+                                </a>
+                            </li>
+                            <li className="py-1 md:py-0" data-menu-id="2590" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/about-us/facilities-hire/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Facilities Hire
+                                </a>
+                            </li>
+                            <li className="py-1 md:py-0" data-menu-id="2599" data-menu-item="inactive">
+
+                                <a href="https://www.putneyhigh.gdst.net/about-us/work-with-us/" target="_self" className="transition-opacity duration-300 ease-in-out hover:opacity-50 md:cursor-pointer">
+                                    Current Vacancies
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex  gap-8 lg:gap-16 justify-center items-center w-full my-16 '>
+
+                    <div className=' '>
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/06/dummy-acc-logo-japs.svg" alt="school-logo1" className='h-12 lg:h-16 ' />
+                    </div>
+
+                    <div className=' '>
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/06/dummy-acc-logo-hmc.svg" alt="school-logo2" className='h-12 lg:h-16' />
+
+                    </div>
+                    <div className='  '>
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/06/dummy-acc-logo-gsa.svg" alt="school-logo3" className='h-12 lg:h-16' />
+
+                    </div>
+                    <div className=' '>
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/06/dummy-acc-logo-the-good-schools-guide.svg" alt="school-logo4" className='h-12 lg:h-16' />
+
+                    </div>
+                    <div className=' '>
+                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/10/gdst-logo-white.svg" alt="school-logo5" className='h-12 lg:h-16' />
+
                     </div>
 
                 </div>
 
-                <div className='flex border border-red-500'>
+                <div className="px-8 py-12 w-full ">
+                    <div className="flex flex-col justify-center gap-8 lg:items-center lg:flex-row-reverse md:gap-16 text-white">
+                        <div className="flex flex-row items-end gap-16 justify-center">
+                            <div className="flex flex-wrap gap-4 gap-y-8">
+                                <a href="https://www.instagram.com/putneyhighschool" target="_blank" className="group flex items-center justify-center transition duration-300 ease-in-out hover:bg-opacity-50 border rounded-full size-12 border-transparent min-w-12 relative bg-white text-base md:text-lg text-primary aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                                    <i className="fa-brands fa-instagram  fa-lg" aria-hidden="true"></i>
+                                    <div className="absolute text-xs font-bold lg:opacity-0 -bottom-7 group-hover:!opacity-100 transition-opacity duration-300 ease-in-out        text-white"></div>
+                                </a>
+                                <a href="https://www.facebook.com/putneyhighschoolGDST/" target="_blank" className="group flex items-center justify-center transition duration-300 ease-in-out hover:bg-opacity-50 border rounded-full size-12 border-transparent min-w-12 relative bg-white text-base md:text-lg text-primary aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+                                <i className="fa-brands fa-instagram" style={{color: "#511f46;"}}></i>
+                                    <div className="absolute text-xs font-bold lg:opacity-0 -bottom-7 group-hover:!opacity-100 transition-opacity duration-300 ease-in-out        text-white"></div>
+                                </a>
+                                <a href="https://www.linkedin.com/school/putney-high-school" target="_blank" className="group flex items-center justify-center transition duration-300 ease-in-out hover:bg-opacity-50 border rounded-full size-12 border-transparent min-w-12 relative bg-white text-base md:text-lg text-primary aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+                                    <i className="fa-brands fa-linkedin-in " aria-hidden="true"></i>
+                                    <div className="absolute text-xs font-bold lg:opacity-0 -bottom-7 group-hover:!opacity-100 transition-opacity duration-300 ease-in-out      "></div>
+                                </a>
+                                <a href="https://www.youtube.com/channel/UCL1HMSQWmj08mnbPs3HRw-w" target="_blank" className="group flex items-center justify-center transition duration-300 ease-in-out hover:bg-opacity-50 border rounded-full size-12 border-transparent min-w-12 relative bg-white text-base md:text-lg text-primary aos-init aos-animate" data-aos="fade-up" data-aos-delay="400">
+                                    <i className="fa-brands fa-youtube fa-lg" aria-hidden="true"></i>
+                                    <div className="absolute text-xs font-bold lg:opacity-0 -bottom-7 group-hover:!opacity-100 transition-opacity duration-300 ease-in-out text-white"></div>
+                                </a>
+                            </div>
+                            <button className="flex items-center justify-center transition duration-300 ease-in-out hover:bg-opacity-50 border rounded-full size-12 border-transparent min-w-12 relative bg-white aos-init aos-animate" data-button-back-to-top="" data-aos="fade-up" data-aos-delay="400">
+                                <i className="fa-light fa-arrow-up text-base md:text-lg text-primary       " aria-hidden="true"></i>
+                            </button>
+                        </div>
+                        <div className="mt-8 md:mt-0 grow text-center lg:text-left prose-copyright aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                            <p className='text-lg'>Copyright 2025 | <a href="https://www.gdst.net/privacy-notice/" target="_blank" rel="noopener">Privacy</a></p>
 
-                    <div className='w-[25%]'>
-                        <img src="https://www.putneyhigh.gdst.net/wp-content/uploads/2024/06/dummy-acc-logo-japs.svg" alt="shool-logo1" className='h-32 w-32' />
+                            <p>Site by <a href="https://www.meandhimdesign.co.uk/" target="_blank">Arindam </a></p>
+                        </div>
                     </div>
-
                 </div>
 
             </div>
